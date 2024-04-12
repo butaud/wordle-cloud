6feb241e9eab822a50a385f691765119813f37c3
Carter Butaud <cabutaud@microsoft.com>
2024-04-12 11:16:30 -0700

    added express server

--- svc/src/server.ts
+++ svc/src/server.ts
@@ -1,1 +1,34 @@
-console.log("hello, wordle!");
+import express from "express";
+import { SqlitePuzzleDao } from "./data/sqliteDao";
+import { Solve } from "./data/interface";
+
+const app = express();
+app.use(express.json());
+
+(async () => {
+  const sqliteDao = new SqlitePuzzleDao("./bin/prod.db");
+  await sqliteDao.init();
+
+  app.get("/todayPuzzle", async (req, res) => {
+    const today = new Date();
+    const puzzleId = await sqliteDao.getPuzzle(today);
+    res.json({ puzzleId });
+  });
+
+  app.get("/solves/:puzzleId", async (req, res) => {
+    const puzzleId = parseInt(req.params.puzzleId);
+    const solves = await sqliteDao.getSolves(puzzleId);
+    res.json(solves);
+  });
+
+  app.post("/solves/:puzzleId", async (req, res) => {
+    const puzzleId = parseInt(req.params.puzzleId);
+    const newSolve: Omit<Solve, "id" | "puzzleId"> = req.body;
+    const addedSolve = await sqliteDao.createSolve(puzzleId, newSolve);
+    res.json(addedSolve);
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+})();
