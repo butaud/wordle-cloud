d39c101975f1442fe82c409cb52b4bcee05f680d
Carter Butaud <cabutaud@microsoft.com>
2024-04-19 15:35:43 -0700

    trying server on port 80

--- svc/src/server.ts
+++ svc/src/server.ts
@@ -29,7 +29,7 @@ app.use(cors());
     res.json(addedSolve);
   });
 
-  app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+  app.listen(80, () => {
+    console.log("Server is running on port 80");
   });
 })();
