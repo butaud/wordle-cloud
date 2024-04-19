56847e2f12acfabc1de4a9626cd4e8583fc52cef
Carter Butaud <cabutaud@microsoft.com>
2024-04-18 22:37:59 -0700

    color shading works

--- web/src/Cloud.tsx
+++ web/src/Cloud.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Solve } from "./data/interface";
+import { Solve, SolveRowItem } from "./data/interface";
 
 import "./Cloud.css";
 
@@ -8,13 +8,15 @@ export type CloudProps = {
 };
 
 export const Cloud: FC<CloudProps> = ({ solves }) => {
-  const tableRows: string[][] = [];
+  const tableRows: SolveRowItem[][][] = [];
+  const rowCounts = [0, 0, 0, 0, 0, 0];
   for (let i = 0; i < 6; i++) {
     tableRows[i] = [];
+    rowCounts[i] = solves.filter((solve) => solve.solveRows[i]).length;
     for (let j = 0; j < 5; j++) {
-      tableRows[i][j] = "";
+      tableRows[i][j] = [];
       for (let solve of solves) {
-        tableRows[i][j] += solve.solveRows[i]?.[j] ?? "";
+        tableRows[i][j].push(solve.solveRows[i]?.[j]);
       }
     }
   }
@@ -24,11 +26,77 @@ export const Cloud: FC<CloudProps> = ({ solves }) => {
         {tableRows.map((row, i) => (
           <tr key={i}>
             {row.map((cell, j) => (
-              <td key={j}>{cell}</td>
+              <SquareCell
+                key={j}
+                items={cell}
+                totalSolves={solves.length}
+                rowSolves={rowCounts[i]}
+              />
             ))}
           </tr>
         ))}
       </tbody>
     </table>
   );
+};
+
+type Point = {
+  hue: number;
+  lightness: number;
+};
+const setPoints: Point[] = [
+  {
+    hue: 60,
+    lightness: 100,
+  },
+  {
+    hue: 60,
+    lightness: 59,
+  },
+  {
+    hue: 90,
+    lightness: 41,
+  },
+];
+const bins = setPoints.length - 1;
+const binDivisor = 2 / bins;
+
+const colorForCell = (
+  items: SolveRowItem[],
+  totalSolves: number,
+  rowSolves: number
+): string => {
+  if (items.length === 0) {
+    return "white";
+  }
+
+  const alpha = rowSolves / totalSolves;
+
+  const cellTotal = (
+    items.map((item) => (item === "G" ? 2 : item === "Y" ? 1 : 0)) as number[]
+  ).reduce((a, b) => a + b, 0);
+  const cellMean = cellTotal / items.length;
+
+  const bin = Math.floor(cellMean / binDivisor);
+  const point1 = setPoints[bin];
+  const point2 = setPoints[bin + 1] ?? point1;
+  const t = (cellMean - bin * binDivisor) / binDivisor;
+  const hue = interpolate(point1.hue, point2.hue, t);
+  const lightness = interpolate(point1.lightness, point2.lightness, t);
+  return `hsla(${hue}, 100%, ${lightness}%, ${alpha})`;
+};
+
+const interpolate = (a: number, b: number, t: number) => {
+  return a + (b - a) * t;
+};
+
+const SquareCell: FC<{
+  items: SolveRowItem[];
+  totalSolves: number;
+  rowSolves: number;
+}> = ({ items, rowSolves, totalSolves }) => {
+  const backgroundColor = colorForCell(items, rowSolves, totalSolves);
+  console.log(items, backgroundColor);
+  const contents = items.map((item) => item || "").join("");
+  return <td style={{ backgroundColor: backgroundColor }}>{contents}</td>;
 };
